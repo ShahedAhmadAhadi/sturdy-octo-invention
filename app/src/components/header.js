258829851e6447e037e349252bf50e0b9b7f3c55
Shahed Ahmad <shaheda.a16@gmail.com>
2021-09-11 17:05:33 +0430

    header properties changed

--- app/src/components/header.js
+++ app/src/components/header.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './styles/header.css'
+import { NavLink } from 'react-router-dom'
 
 function Header() {
     return (
-        <div class="wrapper">
-            <h1>
+        <div className="wrapper">
+            <h1 className="header">
             To-Do
             </h1>
+            <div className="list">
+            <NavLink to="/" activeClassName="">Home</NavLink>
+            <NavLink to="/" activeClassName="">Sign</NavLink>
+            <NavLink to="/" activeClassName="">Home</NavLink>
+            </div>
 
         </div>
     )
