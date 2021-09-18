155edaa9ddf751c199bbec3fab6ca1448299da36
Shahed Ahmad <shaheda.a16@gmail.com>
2021-09-18 22:15:51 +0430

    user authentications sent to firebase by createUserWithEmailAndPassword and then states set to empty after return

--- app/src/components/signUp.js
+++ app/src/components/signUp.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { auth, createUserProfileDocument } from '../firebase/firebase.utils'
 import { useState, useEffect } from 'react'
-function signUp() {
+function SignUp() {
 
     const [displayName, setDisplayName] = useState('')
     const [password, setPassword] = useState('')
@@ -9,17 +9,43 @@ function signUp() {
     const [confirmPassword, setConfirmPassword] = useState('')
     const [image, setImage] = useState('')
 
+    handleSubmit = async event => {
+        event.perventDefault();
+
+        if (password !== confirmPassword) {
+            alert('password not match')
+            return
+        }
+
+        try {
+
+            const { user } = await auth.createUserWithEmailAndPassword(email, password)
+
+            await createUserProfileDocument(user, {displayName, image})
+
+            setEmail('')
+            setImage('')
+            setPassword('')
+            setConfirmPassword('')
+            setDisplayName('')
+            
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" value={displayName} required />
                 <input type="email" value={email} required />
                 <input type="password" value={password} required />
                 <input type="password" value={confirmPassword} required />
                 <input type="file" value={image} required />
+                <button>Sign Up</button>
             </form>
         </div>
     )
 }
 
-export default signUp
+export default SignUp
