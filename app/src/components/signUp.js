8be000da4781202061c11ba0fb7c8af8837d3927
Shahed Ahmad <shaheda.a16@gmail.com>
2021-09-18 20:17:55 +0430

    new fields added for entering data into signup form

--- app/src/components/signUp.js
+++ app/src/components/signUp.js
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import { auth, createUserProfileDocument } from '../firebase/firebase.utils'
+import { useState, useEffect } from 'react'
 function signUp() {
+
+    const [displayName, setDisplayName] = useState('')
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [image, setImage] = useState('')
+
     return (
         <div>
-            
+            <form>
+                <input type="text" value={displayName} required />
+                <input type="email" value={email} required />
+                <input type="password" value={password} required />
+                <input type="password" value={confirmPassword} required />
+                <input type="file" value={image} required />
+            </form>
         </div>
     )
 }
