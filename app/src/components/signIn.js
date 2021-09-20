9b87fa4c17de50c764e621b22107c1c20dc3ea6f
Shahed Ahmad <shaheda.a16@gmail.com>
2021-09-20 20:50:49 +0430

    sign in component sign in without google implemented and tested successfully

--- app/src/components/signIn.js
+++ app/src/components/signIn.js
@@ -1,32 +1,43 @@
 import React from 'react'
 import { useState, userEffect } from 'react';
-import { signInWithGoogle } from '../firebase/firebase.utils';
+import { signInWithGoogle, auth } from '../firebase/firebase.utils';
+import { signInWithEmailAndPassword } from 'firebase/auth'
+
 
 function SignIn() {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    let handleSubmit = e => {
+    const handleSubmit = async e => {
         e.preventDefault();
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password)
+            setPassword('')
+            setEmail('')
+        } catch (error) {
+            console.log(error)
+        }
 
         setEmail('')
         setPassword('')
     }
 
     const handleChangeEmail = event => {
-        const value = event.target
+        const value = event.target.value
+        console.log(event.target.value)
         setEmail(value)
     }
     const handleChangePassword = event => {
-        const value = event.target
+        const value = event.target.value
         setPassword(value)
     }
 
 
 
     return (
-        <form onSubmit={() => handleSubmit()}>
+        <form onSubmit={(e) => handleSubmit(e)}>
             <input type="text" value={email} required placeholder="E-mail" onChange={handleChangeEmail} />
             <input type="password" value={password} required placeholder="password" onChange={handleChangePassword} />
             <input type="submit" value="Submit"/>
