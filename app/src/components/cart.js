5f30c05bfb7bbb76103da5a5715cceac22abf696
Shahed Ahmad <shaheda.a16@gmail.com>
2021-09-22 21:34:39 +0430

    styles arranged in proper way

--- app/src/components/cart.js
+++ app/src/components/cart.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 import {FiShoppingCart} from 'react-icons/fi'
 import './styles/cart.css'
 
 function Cart() {
+
+    const [color, setColor] = useState('#e9dea2')
+
+    const hover = (value) => {
+        setColor(value)
+    }
+
+    console.log(color)
     return (
-        <div className="cart-wrapper">
-            <FiShoppingCart className="cart" />
+        <div className="cart-wrapper" onMouseEnter={() => hover('#840032')} onMouseLeave={() => hover('#e9dea2')}>
+            <FiShoppingCart className="cart" style={{color: color}} />
             <div className="cart-items-num">2</div>
             <div className="cart">
-                
+
             </div>
         </div>
     )
