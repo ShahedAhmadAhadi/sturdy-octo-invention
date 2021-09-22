00663da97ef1a8db08201561a8311ad40228af65
Shahed Ahmad <shaheda.a16@gmail.com>
2021-09-22 21:58:12 +0430

    add_item component fields and styles added

--- app/src/components/add_item.js
+++ app/src/components/add_item.js
@@ -4,7 +4,17 @@ function AddItem() {
     return (
         <div className="add-item-wrapper">
             <div className="add-item-modal">
-                
+                <form>
+                    <input type="text" placeholder="item-name" />
+                    <input type="number" placeholder="price" step=".00" />
+                    <select name="Catagory">
+                        <option value="fruit">Fruit</option>
+                        <option>Fruit</option>
+                        <option>Fruit</option>
+                    </select>
+                    <input type="file" />
+                    <input type="number" placeholder="amount" />
+                </form>
             </div>
         </div>
     )
