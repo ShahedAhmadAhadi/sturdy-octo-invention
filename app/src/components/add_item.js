be5b882aaec07514a334ec32eb20974ea2ee7d8b
Shahed Ahmad <shaheda.a16@gmail.com>
2021-09-22 22:26:37 +0430

    all styles added for add_item and new field for discription

--- app/src/components/add_item.js
+++ app/src/components/add_item.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import './styles/styles.css'
 
 function AddItem() {
     return (
-        <div className="add-item-wrapper">
-            <div className="add-item-modal">
+        <div className="add-item-wrapper" style={{ width: '100%', height: '100vh', background: '#00000033', position: 'absolute', top: 0, left: 0, display: 'flex', justifyContent: 'center' }}>
+            <div className="add-item-modal" style={{ background: '#fff', alignSelf: 'center' }}>
                 <form>
                     <input type="text" placeholder="item-name" />
-                    <input type="number" placeholder="price" step=".00" />
+                    <input type="text" placeholder="discription" />
+                    <input type="number" placeholder="price" step=".01" />
                     <select name="Catagory">
                         <option value="fruit">Fruit</option>
                         <option>Fruit</option>
--- app/src/components/main.js
+++ app/src/components/main.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import AddItem from './add_item'
+import { useState } from 'react'
 
 function Main() {
+
+    const [addItemVisible, setAddItemVisible] = useState(false)
+
+    const addItemShow = () => {
+        setAddItemVisible(true)
+    }
+
     return (
         <div>
             <h1>Main</h1>
-            <button className="btn-add">Add Item</button>
+            <button className="btn-add" onClick={() => addItemShow()}>Add Item</button>
+            {addItemVisible && <AddItem />}
         </div>
     )
 }
