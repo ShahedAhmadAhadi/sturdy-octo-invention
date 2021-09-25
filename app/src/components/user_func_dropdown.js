35216b97b363267050a829a5c366f62cf7052374
Shahed Ahmad <shaheda.a16@gmail.com>
2021-09-25 22:20:39 +0430

    closing dropdown after clicking outside has a bug

--- app/src/components/user_func_dropdown.js
+++ app/src/components/user_func_dropdown.js
@@ -5,9 +5,9 @@ function UserDropdown(props) {
 
     console.log(props)
     return (
-        <div style={{width: '100%', height:'100vh', background: '#000aa', position: 'absolute', top: 0, left:0}}>
+        <div style={{width: '100%', height:'100vh', background: '#000aa', position: 'absolute', top: 0, left:0}} onClick={()=> {props.dropdownToggle()}}>
 
-        <div className='dropdown'>
+        <div className='dropdown' onClick={e => e.stopPropagation()}>
 
             <ul className="dropdown-list">
                 <li className="list-item-0">{props.user.displayName}</li>
