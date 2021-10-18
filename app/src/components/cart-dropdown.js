d18a6c5cae32e56d49887f13eeeebd1a3cea211a
Shahed Ahmad <shaheda.a16@gmail.com>
2021-10-18 21:39:43 +0430

    hover and styles on hovering child component

--- app/src/components/cart-dropdown.js
+++ app/src/components/cart-dropdown.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './styles/cart_dropdown.css'
 
 function CartDropdown() {
     return (
-        <div className="cart-dropdown">
+        <div className="cart-dropdown" onMouseOver={(e) => {e.stopPropagation()}} onMouseUp={(e) => {e.stopPropagation()}} onMouseEnter={(e) => {e.stopPropagation()}} onMouseUpCapture={(e) => {e.stopPropagation()}} onMouseOut={(e) => {e.stopPropagation()}}>
             CartDropdown
         </div>
     )
