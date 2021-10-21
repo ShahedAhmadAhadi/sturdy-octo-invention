b4e7f92246590aaed9bc6474397c5e8f04ff1078
Shahed Ahmad <shaheda.a16@gmail.com>
2021-10-21 19:16:17 +0430

    division and styles added for cart-dropdown

--- app/src/components/cart-dropdown.js
+++ app/src/components/cart-dropdown.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './styles/cart_dropdown.css'
+import { FiArrowRight } from 'react-icons/fi'
 
 function CartDropdown() {
     return (
         <div className="cart-dropdown" >
-            CartDropdown
+            <div className="cart-dropdown-right" style={{padding: '0.1em 1em', width: '10%'}}>
+                <div className="cart-dropdown-right-shape">
+                    <FiArrowRight />
+                </div>
+                <div className="cart-dropdown-right-text">
+                    Items
+                </div>
+                <div className="cart-dropdown-right-shape">
+                    <FiArrowRight />
+                </div>
+            </div>
+            <div></div>
         </div>
     )
 }
