ec1a98b1bed28e453c945a2ba4754aacfa03d39a
Shahed Ahmad <shaheda.a16@gmail.com>
2021-10-21 19:58:06 +0430

    cart dropdown edited

--- app/src/components/cart-dropdown.js
+++ app/src/components/cart-dropdown.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import './styles/cart_dropdown.css'
 import { FiArrowRight } from 'react-icons/fi'
+import {useState} from 'react'
 
 function CartDropdown() {
+    
+    const [color, setColor] = useState('#fff')
+
+    const hover = (value) => {
+        setColor(value)
+    }
+
     return (
         <div className="cart-dropdown" >
-            <div className="cart-dropdown-right" style={{padding: '0.1em 1em', width: '10%'}}>
-                <div className="cart-dropdown-right-shape">
+            <div className="cart-dropdown-left" style={{padding: '0.1em 1em', width: '10%'}}>
+                <div className="cart-dropdown-left-shape">
                     <FiArrowRight />
                 </div>
-                <div className="cart-dropdown-right-text">
+                <div className="cart-dropdown-left-text" style={{color: '#fff'}}>
                     Items
                 </div>
-                <div className="cart-dropdown-right-shape">
+                <div className="cart-dropdown-left-shape">
                     <FiArrowRight />
                 </div>
             </div>
-            <div></div>
+            <div className="cart-dropdown-right">
+                
+            </div>
         </div>
     )
 }
